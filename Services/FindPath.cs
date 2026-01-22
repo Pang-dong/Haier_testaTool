98a41ad8238d265d0a64b476f1d0dddbdd00d62b
pang <2493483136@qq.com>
2026-01-22 19:27:26 +0800

    查找发现烧录文件

---
+++ Services/FindPath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Haier_E246_TestTool.Services
+{
+    public  class FindPath
+    {
+        public string GetFilePathByKeyword( string directory,string keyword,string DefaultFileName)
+        {
+            if(!Directory.Exists(directory))
+            {
+                return Path.Combine(directory,DefaultFileName);
+            }
+            try
+            {
+                var files =Directory.GetFiles(directory,"*"+keyword+"*");
+                if (files.Length > 0) 
+                {
+                    return files[0];
+                }
+                else
+                {
+                    return Path.Combine(directory, DefaultFileName);
+                }
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"查找文件出错 (关键字: {keyword}): {ex.Message}");
+                return Path.Combine(directory, DefaultFileName);
+            }
+        }
+    }
+}
